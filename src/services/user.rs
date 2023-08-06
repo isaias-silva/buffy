a390750e1f3e9e7cc8582ce7164175d43577fa08
isaias-silva <zackblackmangold@gmail.com>
2023-08-05 22:10:47 -0300

    loginm

--- src/services/user.rs
+++ src/services/user.rs
@@ -10,17 +10,15 @@ pub mod user_service {
                 password: Some(_password),
                 name: Some(_name),
             } => {
-
-            
-                   let real_name= env::var("USERNAME").expect("user name not found");
+                   let real_name= env::var("OWNERNAME").expect("user name not found");
                     let real_password=env::var("PASSWORD").expect("password user not found");
-             
-            
                 let name_res=_name.clone(); 
                 let password_res=_password.clone();
-                if real_name==name_res && password_res==real_password{
+            
 
-                    print!("{} = {}",real_name,name_res);
+                if name_res==real_name && password_res==real_password{
+
+
                     return Ok(Response::new::<&str>(&format!("welcome {}!", name_res), None));
                 }else{
                     return Err(Response::new::<&str>(&format!("incorrect password"), None));
